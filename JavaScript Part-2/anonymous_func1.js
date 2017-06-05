e19840946a4600dae092e7f1e88d0511ce3d0c11
coderavana <coderavana@gmail.com>
2017-06-05 21:48:28 +0530

    Working on Anonymous Functions

---
+++ JavaScript Part-2/anonymous_func1.js
@@ -0,0 +1,12 @@
+var people = 35;
+var rain = 4;
+var sharks = 2;
+
+var fearGenerated = function(numPeeps, rainInInches, numSharks) {
+  var rainFear = numPeeps * rainInInches;
+  var sharkFear = numSharks * numSharks * numSharks;
+  var totalFear = sharkFear + rainFear;
+  return totalFear;
+};
+
+var fear = fearGenerated(people,rain,sharks);
